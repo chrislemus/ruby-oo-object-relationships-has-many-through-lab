5446b46f780809c40abc447714d6f9dfbb9d48e6
chrislemus <icrislemus@gmail.com>
2020-12-22 21:11:36 -0500

    commit tree e9e6315a7c0424035f0825904784720490b60467

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,21 @@
+class Genre
+  attr_reader :genre, :name
+  @@all = []
+  def initialize(genre)
+    @genre = genre
+    @name = genre
+    @@all << self
+  end
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
 
+  def artists
+    songs.map {|song| song.artist}
+  end
+
+  def self.all
+    @@all
+  end
+end
