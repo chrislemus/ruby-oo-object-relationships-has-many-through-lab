2ecb751392c44943a17c6aee985477b8c2b9c94f
chrislemus <icrislemus@gmail.com>
2020-12-22 21:11:41 -0500

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,18 @@
+class Appointment
+  attr_reader :date, :patient, :doctor
+  @@all = []
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
 
+  # def new_appointment(date, doctor)
+  #   Appointment.new(date,)
+  # end
+
+  def self.all
+    @@all
+  end
+end
--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,24 @@
+class Artist
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def genres
+    Song.all.map {|song| song.genre}
+  end
+
+  def self.all
+    @@all
+  end
+end
